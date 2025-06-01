8c24ee52b5751ee59c6c9a30dfaecf4f2081819a
mentneo <mentneo6@gmail.com>
2025-06-01 10:44:42 +0530

    add the logo

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['res.cloudinary.com', 'mentneo.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, s-maxage=86400',
+          },
+        ],
+      },
+    ];
+  },
+  // Disable edge runtime temporarily to solve invocation issues
+  experimental: {
+    runtime: 'nodejs',
+    serverComponents: true,
+  },
+};
+
+module.exports = nextConfig;
