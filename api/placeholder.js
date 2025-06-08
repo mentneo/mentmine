c11de7200e4df173713c9dfee618e4d18d6ddeeb
mentneo <mentneo6@gmail.com>
2025-06-08 10:15:22 +0530

    add the mnew api

---
+++ api/placeholder.js
@@ -0,0 +1,4 @@
+// This is a placeholder API route to prevent Vercel deployment errors
+export default function handler(req, res) {
+  res.status(200).json({ status: 'ok', message: 'API is working' });
+}
