7425471eddcb76e24c73a24e2ffefbd0b958a471
mentneo <mentneo6@gmail.com>
2025-06-10 16:03:57 +0530

    Api errors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-module.exports = (req, res) => {
+// This is a simple API handler
+export default function handler(req, res) {
   res.status(200).json({
     status: 'ok',
     message: 'API is working'
   });
-};
+}
